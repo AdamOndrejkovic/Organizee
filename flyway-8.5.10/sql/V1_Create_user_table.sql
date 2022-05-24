461f14db65b7c613ba4340ebe211e8f75e5f115d
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-05-24 20:21:46 +0200

    Finishing touches

--- flyway-8.5.10/sql/V1_Create_user_table.sql
+++
@@ -1,9 +0,0 @@
-create table Users
-(
-    Id             INTEGER not null
-        constraint PK_Users
-            primary key autoincrement,
-    Name           TEXT,
-    HashedPassword TEXT,
-    Salt           TEXT
-);
